8e5965c27a705c1300727df93f17e8db4a45f8c6
Sk Mijanul Haque <52299208+mijanul@users.noreply.github.com>
2026-06-01 17:14:34 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b); // SORTING REQUIRED FOR TWO POINTER
+
+  const result = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break; // sorted array so impossible to reach 0
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      // MOST IMPORTANT TO SKIP DUPLICATE
+      continue;
+    }
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left] === nums[left + 1]) {
+          // SKIP DUPLICATE
+          left++;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          // SKIP DUPLICATE
+          right--;
+        }
+
+        left++;
+        right--;
+      } else if (sum > 0) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+
+  return result;
+
+                           }
+}
