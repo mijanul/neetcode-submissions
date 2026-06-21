6b1d1735eb25c5b2815111eeb8eec8f505ba8d68
Sk Mijanul Haque <52299208+mijanul@users.noreply.github.com>
2026-06-21 22:05:33 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        let result: number = 0;
+  let left = 0;
+  let maxFreq = 0;
+
+  let count: Record<string, number> = {};
+
+  for (let right = 0; right < s.length; right++) {
+    count[s[right]] = (count[s[right]] || 0) + 1;
+
+    maxFreq = Math.max(maxFreq, count[s[right]]);
+
+    while (right - left + 1 - maxFreq > k) {
+      count[s[left]]--;
+      left++;
+    }
+
+    result = Math.max(result, right - left + 1);
+  }
+
+  return result;
+    }
+}
