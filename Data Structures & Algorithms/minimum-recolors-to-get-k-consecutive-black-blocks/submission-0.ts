06315f859518470412c2a17d4be6d477644794d7
Sk Mijanul Haque <52299208+mijanul@users.noreply.github.com>
2026-06-22 23:51:14 +0530

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-0

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-0.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} blocks
+     * @param {number} k
+     * @return {number}
+     */
+    minimumRecolors(blocks: string, k: number): number {
+        let res: number = Infinity;
+
+        for (let i = 0; i < blocks.length; i++) {
+            if (!blocks[i + k - 1]) continue;
+            let count = 0;
+            let right = 0;
+
+            while (right < k) {
+                if (blocks[i + right] === "W") {
+                    count++;
+                }
+                right++;
+            }
+
+            res = Math.min(res, count);
+        }
+
+        return res;
+    }
+}
