c2dbd2e6119df8169bc6071a56c430ea02121436
Sk Mijanul Haque <52299208+mijanul@users.noreply.github.com>
2026-07-02 22:08:20 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let maxProfit = 0;
+        let minPrice = prices[0];
+
+        for (let i = 1; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+
+            let profit = prices[i] - minPrice;
+
+            maxProfit = Math.max(maxProfit, profit);
+        }
+
+        return maxProfit;
+    }
+}
