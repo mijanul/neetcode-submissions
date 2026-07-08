6ebad53319f76a01a37cd6212f179696e22a5b63
Sk Mijanul Haque <52299208+mijanul@users.noreply.github.com>
2026-07-08 20:22:46 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const result: number[] = new Array(nums.length).fill(1);
+
+        let prefix = 1;
+
+        for (let i = 0; i < nums.length; i++) {
+            result[i] *= prefix;
+            prefix *= nums[i];
+        }
+
+        let postfix = 1;
+
+        for (let j = nums.length - 1; j >= 0; j--) {
+            result[j] *= postfix;
+            postfix *= nums[j];
+        }
+
+        return result;
+    }
+}
