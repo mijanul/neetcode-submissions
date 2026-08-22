eb75a79c24bfc3b94faa966cf8d058d612805d3e
Sk Mijanul Haque <52299208+mijanul@users.noreply.github.com>
2026-08-23 00:55:32 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
